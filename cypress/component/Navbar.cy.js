67993f81fac0722652fb64c33e7ead0b2a0499c4
hcone5006 <heidicone@gmail.com>
2022-09-12 22:00:20 +1200

    added more tests for Navbar component

--- cypress/component/Navbar.cy.js
+++ cypress/component/Navbar.cy.js
@@ -1,9 +1,26 @@
 import { Navbar } from "../../src/Components/Navbar.jsx";
+import { Paragraph } from "../../src/Components/Paragraph.jsx";
+
+const testNav = <Navbar bg="primary"><Paragraph size="lg">Test App</Paragraph><Paragraph size="sm">Login</Paragraph></Navbar>;
 
 describe('Navbar.cy.js', () => {
   it('render navbar', () => {
-    cy.mount(<Navbar bg="primary"><p>Test App</p></Navbar>)
-    cy.get('p').contains( 'Test App')
-    // cy.get('nav').contains('have.class', 'bg-primary')
+    cy.mount(testNav)
+    cy.get('nav').should('be.visible')
+    
+  })
+
+  it('renders nav text', () => {
+    cy.mount(testNav)
+    cy.get('p:first-child').contains( /Test App/)
+    cy.get('p:nth-child(2)').contains( /Login/)
+  })
+
+  it('nav text styled', () => {
+    cy.mount(testNav)
+    cy.get('p:first-child').should('have.css', 'font-size', '35.2px')
+    cy.get('p:first-child').should('have.css', 'font-weight', '600')
+    cy.get('p:nth-child(2)').should('have.css', 'font-size', '16px')
+    cy.get('p:nth-child(2)').should('have.css', 'font-weight', '400')
   })
 })
